ef1f66ae330b8f40248027cb11dff89c91646ea4
Ashank <ashankbekkam@gmail.com>
2021-08-10 08:31:13 +0530

    first commit

---
+++ src/Recomendations.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Appbar from './Components.js/Appbar'
+
+const Recomendations = () => {
+    return (
+        <div>
+            <Appbar/>
+            
+            
+            
+        </div>
+    )
+}
+
+export default Recomendations
